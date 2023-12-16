b2c9d58e4f2d2f7290e1d5c204226efb845b67db
Ahmad Zaki <ahmdzakiiiy@gmail.com>
2023-12-16 21:31:28 +0700

    update game logic

--- public/script.js
+++ public/script.js
@@ -1,38 +1,163 @@
+
+// Game Started
+
 const showModal = document.getElementById('showModal');
 const modal = document.getElementById('modal');
 const main = document.getElementById('main');
+const game = document.getElementById('game');
 
 showModal.addEventListener('click', () => {
-    modal.classList.remove('hidden');
+    game.classList.remove('hidden');
     main.classList.add('invisible');
 })
 
 
+
 // Game Rules
 
-const player = document.getElementById('input');
+let chance = 3;
+let result;
 
-let cpu = Math.round(Math.random() * 10);
+btn.addEventListener('click', function() {
+    play();
+});
+let number = Math.floor(Math.random() * 10);
+let inputNumber = document.getElementById('input').value;
+let message = document.getElementById('message');
 
-let alert = document.getElementById('alert');
+function play() {
 
-let result = ''
+   
+        
+    if (inputNumber == number) {
+        message.innerHTML = "Congratulations! You guessed the correct number.";
+        result = 'WIN';
 
-for (let chance = 3; chance >= 1; chance--) {
-    if (player == cpu) {
-        hasil = "WIN";
+    } else if (inputNumber != number && chance > 0) {
+        
+        chance--;
 
-        break;
-    } else if (player != cpu && chance > 1) {
-        if (player > cpu) {
-            alert = `TERLALU BESAR \nMasih ada ${chance - 1} kesempatan lagi`;
+         if (inputNumber > number) {
+            message.innerHTML = `TERLALU BESAR! \n Kamu punya ${chance} Kesempatan lagi.`;
 
-        } else if (player < cpu) {
-            alert = `TERLALU RENDAH \nMasih ada ${chance - 1} kesempatan lagi`;
+        } else if (inputNumber < number) {
+            message.innerHTML =  `TERLALU RENDAH! \n Kamu punya ${chance} Kesempatan lagi.`;
 
         }
+        
+    } else if (inputNumber != number && chance == 0) {
+        result = 'LOSE'
+        message.innerHTML = "Sorry, you have reached the maximum attempts! Try again. \n Jawaban yang benar adalah " + number;
+        
+    } else {
+        message.textContent = 'ITU BUKAN ANGKA';
     }
-}
 
-console.log(player);
+    console.log(chance);
+    console.log(inputNumber);
+    console.log(number);
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const btn = document.getElementById('btn');
+// let message = document.getElementById('message');
+// let userInput = document.getElementById('input').value;
+// let player = parseInt(userInput);
+// let cpu = Math.ceil(Math.random() * 10);
+// let chance = 3;
+// message.textContent = chance;
+
+
+// // Game Rules
+
+// for (chance = 3; chance > 0; chance--) {
+
+//     if (player != cpu && chance >1) {
+//         if (player > cpu) {
+//             message.textContent = `TERLALU BESAR!`;
+
+//         } else if (player < cpu) {
+//             message.textContent = `TERLALU RENDAH! \nMasih ada ${chance - 1} kesempatan lagi`;
+//         }   
+//     } else if (player != cpu && chance == 1) {
+//         result = lose;
+        
+//     } else if (player == cpu) {
+//         result = won;
+
+//     } else {
+//         message.textContent = "ITU BUKAN ANGKA!"
+//     }
+
+//     chance--;
+// }
+
+
+
+
+
+
+// Game Rules
+
+// let result = '';
+
+// while (chance > 0) {
+
+//     if (player != cpu && chance > 1) {
+//         if (player > cpu) {
+//             message.textContent = `TERLALU BESAR \nMasih ada ${chance - 1} kesempatan lagi`;
+
+//         } else if (player < cpu) {
+//             message.textContent = `TERLALU RENDAH \nMasih ada ${chance - 1} kesempatan lagi`;
+
+//         } else {
+//             message.textContent = `ITU BUKAN ANGKA!`;
+//         }
+
+
+//     } else if (player != cpu && chance == 1) {
+//         result = "LOSE";
+//         message.textContent = `ANDA KALAH \n Jawaban yang benar adalah ${cpu}`
+//         break;
+
+//     } else if (player == cpu) {
+//         result = "WON";
+//         message.textContent = "ANDA MENANG";
+//         break;
+
+//     }
+//     chance--;
+// }
+
+
+
+
 
